3ad23385bfa90664f5ca6870c4422e950dbbc7ff
ChristyLeung <ChristyLeung@users.noreply.github.com>
2018-03-30 15:42:28 +0800

    Add files via upload

--- Machine Learning Foundation/Create a Movie Website/entertainment_center.py
+++ Machine Learning Foundation/Create a Movie Website/entertainment_center.py
@@ -14,8 +14,15 @@ Lucy = media.Movie("Lucy",
 	                 "Everything is connected and existence is only proven through time.",
 	                 "https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Lucy_%282014_film%29_poster.jpg/220px-Lucy_%282014_film%29_poster.jpg",
 	                 "http://player.youku.com/player.php/sid/XNjkzNzkyNjA0/v.swf")
+
+"""
 Harry_Potter.show_trailer()
+Divergent.show_trailer()
+Lucy.show_trailer()
+"""
+
 #movies = [Harry_Potter, Divergent, Lucy] #建立电影列表
 #fresh_tomatoes.open_movies_page(movies) #调用fresh_tomatoes中函数创建网页
 
+movies = [Harry_Potter, Divergent, Lucy]
 fresh_tomatoes.open_movies_page(movies)
