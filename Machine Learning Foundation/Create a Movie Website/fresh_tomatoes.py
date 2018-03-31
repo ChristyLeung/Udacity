8ae9fd9ca485f298b1f7c180642a0b600682b96a
ChristyLeung <ChristyLeung@users.noreply.github.com>
2018-03-31 17:43:49 +0800

    Update fresh_tomatoes.py

--- Machine Learning Foundation/Create a Movie Website/fresh_tomatoes.py
+++ Machine Learning Foundation/Create a Movie Website/fresh_tomatoes.py
@@ -121,7 +121,7 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer_src="{trailer_src}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h3>{movie_title}</h3>
 </div>
 '''
 
