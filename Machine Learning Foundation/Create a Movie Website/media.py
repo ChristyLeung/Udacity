57ec7595f2a3b9cfaeb43009c13cd6b688240685
ChristyLeung <ChristyLeung@users.noreply.github.com>
2018-03-31 16:14:26 +0800

    Update media.py

--- Machine Learning Foundation/Create a Movie Website/media.py
+++ Machine Learning Foundation/Create a Movie Website/media.py
@@ -5,7 +5,7 @@ class Movie():
 		self.title = movie_title
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
+		self.trailer_url = trailer_youtube
 	#instance method
 	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+		webbrowser.open(self.trailer_url)
